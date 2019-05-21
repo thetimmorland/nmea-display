aba2eefbc0c666f2aedf3e0b567b334829f164ac
Timothy John Morland <thetimmorland@gmail.com>
2019-05-21 15:17:52 -0400

    refactored to make use of message handlers

--- nmea-display.ino
+++ nmea-display.ino
@@ -1,15 +1,22 @@
+#include <NMEA0183.h>
+#include <NMEA0183Msg.h>
 #include <LiquidCrystal.h>
-#include "NMEA0183/NMEA0183.h"
+
+#include "nmea-handler.h"
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
+tNMEA0183Msg NMEA0183Msg;
+tNMEA0183 NMEA0183;
+
 void setup() {
+  NMEA0183.SetMsgHandler(DisplayNMEA0183Msg);
+
   Serial.begin(9600);
-  lcd.begin(16, 2);
+  NMEA0183.SetMessageStream(&Serial);
+  NMEA0183.Open();
 }
 
 void loop() {
-  if (Serial.available()) {
-    lcd.write(Serial.read());
-  }
+  NMEA0183.ParseMessages();
 }
---
+++ nmea-handler.cpp
@@ -0,0 +1,6 @@
+#include <NMEA0183.h>
+#include <NMEA0183Msg.h>
+
+void DisplayNMEA0183Msg(const tNMEA0183Msg &NMEA0183Msg) {
+  
+}
